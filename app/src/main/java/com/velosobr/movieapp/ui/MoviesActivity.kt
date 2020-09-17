e21e3e7b3a9a2235f3dffe08ea910ab230a4b7bf
velosobr <linoc.veloso@gmail.com>
2020-09-16 21:57:39 -0300

    [WIP] Adicionando Toolbar e recyclerView

--- app/src/main/java/com/velosobr/movieapp/ui/MoviesActivity.kt
+++ app/src/main/java/com/velosobr/movieapp/ui/MoviesActivity.kt
@@ -1,11 +1,12 @@
-package com.velosobr.movieapp
+package com.velosobr.movieapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.velosobr.movieapp.R
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
     }
 }
