0322f394a979c6be7f150a5e9e24010ea66eb3f0
velosobr <linoc.veloso@gmail.com>
2020-09-18 20:33:03 -0300

    Recycler working with mocked movies list
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- app/src/main/java/com/velosobr/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/velosobr/movieapp/data/model/Movie.kt
@@ -1,22 +1,7 @@
 package com.velosobr.movieapp.data.model
 
-data class Movie(var page: Int,
-                 var keywords: List<Keyword>? = ArrayList(),
-                 var videos: List<Video>? = ArrayList(),
-                 var reviews: List<Review>? = ArrayList(),
-                 val poster_path: String?,
-                 val adult: Boolean,
-                 val overview: String,
-                 val release_date: String,
-                 val genre_ids: List<Int>,
-                 val id: Int,
-                 val original_title: String,
-                 val original_language: String,
-                 val title: String,
-                 val director: String,
-                 val backdrop_path: String?,
-                 val popularity: Float,
-                 val vote_count: Int,
-                 val video: Boolean,
-                 val vote_average: Float)
-
+data class Movie(
+        val title: String,
+        val director: String,
+        val release_date: String,
+)
--- app/src/main/java/com/velosobr/movieapp/ui/MoviesActivity.kt
+++ app/src/main/java/com/velosobr/movieapp/ui/MoviesActivity.kt
@@ -2,7 +2,11 @@ package com.velosobr.movieapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.velosobr.movieapp.R
+import com.velosobr.movieapp.data.model.Movie
+import com.velosobr.movieapp.ui.movies.MoviesAdapter
 import kotlinx.android.synthetic.main.activity_movies.*
 
 class MoviesActivity : AppCompatActivity() {
@@ -13,6 +17,35 @@ class MoviesActivity : AppCompatActivity() {
         toolbarMain.title = getString(R.string.movies_title)
 
         setSupportActionBar(toolbarMain)
-        
+
+        //with para evitar ficar chamando o recyclerMovies muitas vezes
+        with(recyclerMovies) {
+            layoutManager = LinearLayoutManager(this@MoviesActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = MoviesAdapter(getMoviesMocked())
+
+        }
+
+    }
+
+    fun getMoviesMocked(): List<Movie> {
+        return listOf(
+                Movie(title = "A volta dos que não foram", release_date = "2010", director = "Steve Baulmann"),
+                Movie(title = "So Much Love to Give", release_date = "2020", director = "Marcos Carnevale"),
+                Movie(title = "Dark Desire ", release_date = "2010", director = "Leticia López Margalli"),
+                Movie(title = "Missão Greyhound", release_date = "2020", director = "Aaron Schneider"),
+                Movie(title = "La hora de Salvador Romero", release_date = "2017", director = "Gonzalo Gonzalez"),
+                Movie(title = "A volta dos que não foram", release_date = "2010", director = "Steve Baulmann"),
+                Movie(title = "So Much Love to Give", release_date = "2020", director = "Marcos Carnevale"),
+                Movie(title = "Dark Desire ", release_date = "2010", director = "Leticia López Margalli"),
+                Movie(title = "Missão Greyhound", release_date = "2020", director = "Aaron Schneider"),
+                Movie(title = "La hora de Salvador Romero", release_date = "2017", director = "Gonzalo Gonzalez"),
+                Movie(title = "A volta dos que não foram", release_date = "2010", director = "Steve Baulmann"),
+                Movie(title = "So Much Love to Give", release_date = "2020", director = "Marcos Carnevale"),
+                Movie(title = "Dark Desire ", release_date = "2010", director = "Leticia López Margalli"),
+                Movie(title = "Missão Greyhound", release_date = "2020", director = "Aaron Schneider"),
+                Movie(title = "La hora de Salvador Romero", release_date = "2017", director = "Gonzalo Gonzalez")
+
+        )
     }
 }
