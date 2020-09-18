bdc29d741e00414f52e7bf409933c0598f939a35
velosobr <linoc.veloso@gmail.com>
2020-09-17 22:53:14 -0300

    Working in toolbar

--- app/src/main/java/com/velosobr/movieapp/ui/MoviesActivity.kt
+++ app/src/main/java/com/velosobr/movieapp/ui/MoviesActivity.kt
@@ -3,10 +3,16 @@ package com.velosobr.movieapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.velosobr.movieapp.R
+import kotlinx.android.synthetic.main.activity_movies.*
 
 class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
+
+        toolbarMain.title = "Movies"
+
+        setSupportActionBar(toolbarMain)
+        
     }
 }
